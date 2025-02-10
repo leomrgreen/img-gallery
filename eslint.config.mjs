476a0ed234b5f394c80bf49f6a99cb6caa30c675
Leo Jonsson <146437864+leomrgreen@users.noreply.github.com>
2025-02-10 17:39:14 +0100

    test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,4 +13,17 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
-export default eslintConfig;
+const config = {
+  rules: {
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-wrapper-object-types": "off",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "react-hooks/exhaustive-deps": "warn",
+  },
+};
+
+// Combine ESLint extends and custom rules
+export default {
+  ...eslintConfig,
+  ...config,
+};
